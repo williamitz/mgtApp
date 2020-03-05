7dc7c99feb71cd63078aed363233d9a6370f2563
wcallegmt <developer06.gmt@gmail.com>
2020-03-05 18:45:30 -0500

    Primer push

---
+++ src/app/pages/home/home-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import { ComponentsModule } from '../../components/components.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes),
+    ComponentsModule
+  ],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
