4a6d9bbb3465bdf496c1fe16f6b1ebb196e7ce2e
wcallegmt <developer06.gmt@gmail.com>
2020-03-09 18:26:18 -0500

    Actualizar perfil

--- src/app/pages/profile-edit/profile-edit.page.ts
+++ src/app/pages/profile-edit/profile-edit.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+import { ProfileModel } from 'src/app/models/profile.model';
+import { NgForm } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-edit',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileEditPage implements OnInit {
 
-  constructor() { }
+  dayNames = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+  bodyProfile: ProfileModel;
+
+  constructor(private profileSvc: ProfileService, private loadingCtrl: LoadingController, private router: Router) { }
 
   ngOnInit() {
+    this.bodyProfile = new ProfileModel();
+
+    this.profileSvc.onGetProfile().subscribe( (res: any) => {
+      if (!res.ok) {
+        throw new Error( res.error );
+      }
+
+      this.bodyProfile.name = res.data.name;
+      this.bodyProfile.surname = res.data.surname;
+      this.bodyProfile.name = res.data.name;
+      this.bodyProfile.email = res.data.email;
+      this.bodyProfile.phone = res.data.phone;
+      this.bodyProfile.sex = res.data.sex;
+      this.bodyProfile.aboutMe = res.data.aboutMe || '';
+      this.bodyProfile.dateBorn = res.data.dateBorn || '2002-09-23T15:03:46.789';
+
+      // console.log(res);
+    });
+  }
+
+  async onUpdateProfile( frm: NgForm ) {
+
+    if (frm.valid) {
+      await this.onShowLoading();
+      this.profileSvc.onUpdateProfile( this.bodyProfile ).subscribe( async (res: any) => {
+
+        if (!res.ok) {
+          throw new Error( res.error );
+        }
+
+        this.loadingCtrl.dismiss();
+  
+        this.router.navigateByUrl('/profile');
+
+      });
+    }
+
+  }
+
+  async onShowLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Guardando',
+      translucent: true,
+      spinner: 'bubbles'
+    });
+
+    return loading.present();
   }
 
 }
--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProfile } from '../../interfaces/profile.interface';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  
+  dataProfile: IProfile = {
+    name: '',
+    surname: '',
+    nameComplete: '',
+    email: '',
+    phone: '',
+    aboutMe: '',
+    sex: '',
+    dateBorn: ''
+  };
 
-  constructor() { }
+  constructor( private profileSvc: ProfileService ) { }
 
   ngOnInit() {
+    
+    this.profileSvc.onGetProfile().subscribe( (res: any) => {
+      if (!res.ok) {
+        throw new Error( res.error );
+      }
+
+      this.dataProfile.name = res.data.name;
+      this.dataProfile.surname = res.data.surname;
+      this.dataProfile.nameComplete = res.data.nameComplete;
+      this.dataProfile.email = res.data.email;
+      this.dataProfile.phone = res.data.phone;
+      this.dataProfile.aboutMe = res.data.aboutMe;
+      this.dataProfile.sex = res.data.sex;
+      this.dataProfile.dateBorn = res.data.dateBorn;
+
+    });
+
   }
 
 }
